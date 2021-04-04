ea8bd97f7e4d195eb19e8566cad79383fb818f82
Edilberto Sergio Valle Ortiz <edilberto.sergio.valle.ortiz@gmail>
2021-04-04 13:35:54 -0500

    Se agrego el codigo principal

---
+++ giroMatriz.py
@@ -0,0 +1,39 @@
+flecha2 = [["-","X"],
+           ["X","-"]]
+
+flecha3 = [["O","-","X","X"],
+           ["-","X","+","X"],
+           ["X","+","X","-"],
+           ["X","X","-","Q"]]
+
+flecha1 = [["-","-","-","-","-","-","X","X"],
+           ["-","-","-","-","-","X","X","-"],
+           ["X","-","-","-","X","X","-","-"],
+           ["X","-","-","X","X","-","-","-"],
+           ["X","-","X","X","-","-","-","-"],
+           ["X","X","X","-","-","-","-","-"],
+           ["X","X","-","-","-","-","-","-"],
+           ["X","X","X","X","X","X","-","-"]]
+
+def girar(flecha,giros):
+    nuevaFlecha = []
+    for i in range(len(flecha)):
+        nuevaFlecha.append([])
+        for s in range(len(flecha)):
+            nuevaFlecha[i].append(flecha[len(flecha)-(s+1)][i])
+
+    print("======================================")
+    for fila in nuevaFlecha:
+        print(fila)
+
+    if giros<=1:
+        return 0
+    else:
+        girar(nuevaFlecha,giros-1)
+
+def girarFlecha(flecha,cantidadGiros):
+    for fila in flecha:
+        print(fila)
+    girar(flecha,cantidadGiros)
+
+girarFlecha(flecha1,3)
